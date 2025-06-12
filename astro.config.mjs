0d35188655ad7db6d0bf5a7516b8d56db190d96e
Desarrollo1 <desarrollo1@centrojuridicointernacional.com>
2025-06-12 11:45:40 -0500

    add react and  antd library

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,21 @@ import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
